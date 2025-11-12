71fbed5391302e306b7856bdc4eaaefaead351bb
piringerm <piringerm@301-15>
2025-11-12 08:13:27 +0100

    mysql letöltés és connect osztály elkészült

---
+++ connect.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application_Piringer_Mark
+{
+    internal class connect
+    {
+        public MySqlConnection _connection;
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string _connectionString;
+        public connect()
+        {
+            _host = "localhost";
+            _db = "application";
+            _user = "root";
+            _password = "";
+
+            _connectionString = $"SERVER={_host};DATABASE={_db};UID={_user};PASSWORD={_password};SslMode=None";
+
+            _connection = new MySqlConnection(_connectionString);
+        }
+    }
+}
